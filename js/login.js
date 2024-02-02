38b7e9ffe7ac1cf22aa95578f6218c74ef4205b9
Jino0403 <jinho9203@naver.com>
2024-02-02 23:25:14 +0900

    feat: 로컬스토리지를 사용한 로그인 기능 구현완료

---
+++ js/login.js
@@ -0,0 +1,35 @@
+const loginForm = document.querySelector('#login-form')
+const loginInput = loginForm.querySelector('input')
+const hello = document.querySelector('#hello')
+
+const HIDDEN_CLASSNAME = 'hidden'
+const USERNAME_KEY = 'username'
+
+function reload() {
+  window.location.reload()
+}
+
+function handleLogin(event) {
+  event.preventDefault()
+  loginForm.classList.add(HIDDEN_CLASSNAME)
+  const username = loginInput.value
+  localStorage.setItem(USERNAME_KEY, username)
+  greeting(username)
+  window.location.reload()
+}
+
+function greeting(username) {
+  hello.innerText = `환영합니다. ${username}님`
+  hello.classList.remove(HIDDEN_CLASSNAME)
+}
+
+const savedUserName = localStorage.getItem(USERNAME_KEY)
+
+console.log(savedUserName)
+
+if (savedUserName === null) {
+  loginForm.classList.remove(HIDDEN_CLASSNAME)
+  loginForm.addEventListener('submit', handleLogin)
+} else {
+  greeting(savedUserName)
+}
